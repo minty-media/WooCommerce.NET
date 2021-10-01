65194c79a5b3ff78ce85b057ef1532442431de7d
Menno van Leeuwen <menno@vleeuwen.me>
2021-10-01 11:33:56 +0200

    Added test project and Orders functions

---
+++ WooCommerce.NET.Test/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WooCommerce.NET.Test
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            WooCommerce wooCommerce = new WooCommerce("https://mintybol-api.myio.nl",
+                "ck_ebf92e16e4e44edd6b6bbaa17f91da9900bb6a5b", "cs_1eab44a4c963690e870765b998cb625e99b026bb");
+            
+            Order order = await wooCommerce.Orders.Create(new Order()
+            {
+                payment_method = "Bol.com",
+                payment_method_title = "Bol.com koppeling",
+                set_paid = true,
+                status = "completed",
+                billing = new CustomerInfo()
+                {
+                  first_name = "Pawel",
+                  last_name = "TEST",
+                  address_1 = "Mollerusweg 82",
+                  city = "Haarlem",
+                  postcode = "2020 AB",
+                  country = "NL",
+                  company = "Minty Media"
+                },
+                shipping = new CustomerInfo()
+                {
+                  first_name = "Pawel",
+                  last_name = "TEST",
+                  address_1 = "Mollerusweg 82",
+                  city = "Haarlem",
+                  postcode = "2020 AB",
+                  country = "NL",
+                  company = "Minty Media"
+                },
+                line_items = new List<LineItem>()
+                {
+                  new()
+                  {
+                    product_id = 25,
+                    quantity = 4
+                  },
+                  new()
+                  {
+                    product_id = 18,
+                    quantity = 2
+                  }
+                },
+                meta_data = new List<MetaData>()
+                {
+                    new ()
+                    {
+                        key = "_btw",
+                        value = "dsfghjk"
+                    }
+                }
+            });
+            
+        }
+    }
+}
--- WooCommerce.NET/_Orders.cs
+++ WooCommerce.NET/_Orders.cs
@@ -16,31 +16,35 @@ namespace WooCommerce.NET
             this._wooCommerce = _wooCommerce;
         }
 
-        public async Task<Order> Create(Order order)
+        public async Task<Order> Create(Order order, bool dumpJson = false)
         {
             var client = new HttpClient();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Post,
-                RequestUri = new Uri($"{_wooCommerce.host}/orders?consumer_key={_wooCommerce.key}&consumer_secret={_wooCommerce.secret}"),
+                RequestUri = new Uri($"{_wooCommerce.host}/wp-json/wc/v3/orders?consumer_key={_wooCommerce.key}&consumer_secret={_wooCommerce.secret}"),
                 Content = new StringContent(JsonSerializer.Serialize(order, new JsonSerializerOptions(){ WriteIndented = false }))
                 {
                     Headers =
                     {
                         ContentType = new MediaTypeHeaderValue("application/json")
                     }
-                },
+                }
             };
-            request.Headers.UserAgent.Add(new ProductInfoHeaderValue(_wooCommerce.userAgent));
+            
+            if (dumpJson)
+                Console.WriteLine(JsonSerializer.Serialize(order, new JsonSerializerOptions(){ WriteIndented = true }));
+            
+            if (!string.IsNullOrEmpty(_wooCommerce.userAgent))
+                request.Headers.UserAgent.Add(new ProductInfoHeaderValue(_wooCommerce.userAgent));
             
             using (var response = await client.SendAsync(request))
             {
                 if (response.IsSuccessStatusCode)
                     return JsonSerializer.Deserialize<Order>(await response.Content.ReadAsStringAsync());
                 
-                else
-                    Debug.WriteLine($"Failed creating order on WooCommerce:\n - Status code: {response.StatusCode}\n - Reason: {response.ReasonPhrase}\n\n{await response.Content.ReadAsStringAsync()}");
-                
+                Console.WriteLine($"Failed creating order on WooCommerce:\n - Status code: {response.StatusCode}\n - Reason: {response.ReasonPhrase}\n - Response text: {await response.Content.ReadAsStringAsync()}");
+                Console.WriteLine(" - Input towards WooCommerce: " + JsonSerializer.Serialize(order, new JsonSerializerOptions(){ WriteIndented = true }));
             }
             return null;
         }
