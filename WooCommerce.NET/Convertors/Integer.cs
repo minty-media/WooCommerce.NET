c85956d357df434b87fd598228c256665d9e9e70
Menno van Leeuwen <menno@vleeuwen.me>
2021-10-08 20:15:18 +0200

    Handle integers that are of JsonType NULL

--- WooCommerce.NET/Convertors/Integer.cs
+++ WooCommerce.NET/Convertors/Integer.cs
@@ -5,15 +5,18 @@ using System.Text.Json.Serialization;
 
 namespace WooCommerce.NET.Convertors
 {
-    public class IntegerJsonConvertor : JsonConverter<int?>
+    public class IntegerJsonConvertor : JsonConverter<Int32>
     {
-        public override int? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override Int32 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
+            if (reader.TokenType == JsonTokenType.Null)
+                return 0;
+            
             bool s = reader.TryGetInt32(out int value);
             return s ? value : 0;
         }
         
-        public override void Write(Utf8JsonWriter writer, int? value, JsonSerializerOptions options) =>
-            writer.WriteNumberValue(value == null ? 0 : (int)value);
+        public override void Write(Utf8JsonWriter writer, Int32 value, JsonSerializerOptions options) =>
+            writer.WriteNumberValue(value == null ? 0 : value);
     }
 }
